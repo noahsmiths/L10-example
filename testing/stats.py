2cb0c86b8abf96ba416239b6d8a5b200bc78a5a8
noah <90880131+noahsmiths@users.noreply.github.com>
2024-10-30 13:42:24 -0400

    Edit testing

--- testing/stats.py
+++ testing/stats.py
@@ -1,15 +1,14 @@
 from typing import List
 
-
-def sort_list(list: List[int]) -> List[int]:
+def sort_list_ascending(list: List[int]) -> List[int]:
     for i in range(len(list)):
-        for j in range(i, len(list) - 1):
+        for j in range(len(list) - 1):
             if list[j] < list[j + 1]:
                 list[j], list[j + 1] = list[j + 1], list[j]
     
     return list
 
 def find_median(list: List[int]):
-    sorted_test = sort_list(list)
+    sorted_test = sort_list_ascending(list)
 
     return sorted_test[len(sorted_test) // 2]
--- testing/test_stats.py
+++ testing/test_stats.py
@@ -1,9 +1,19 @@
-from stats import sort_list, find_median
+from stats import sort_list_ascending, find_median
 
-def test_sort_list():
+def test_sort_list_ascending():
     list = [5, 0, 9, 1, 2, 3]
-    assert sort_list(list) == [0, 1, 2, 3, 5, 9]
+    sorted_list = sort_list_ascending(list)
 
-def test_find_median():
+    assert sorted_list == [0, 1, 2, 3, 5, 9]
+
+def test_find_median_even_amount():
     list = [5, 0, 9, 1, 2, 3]
-    assert find_median(list) == 2.5
+    median = find_median(list)
+
+    assert median == 2.5
+
+def test_find_median_odd_amount():
+    list = [5, 0, 9, 1, 2]
+    median = find_median(list)
+
+    assert median == 2
